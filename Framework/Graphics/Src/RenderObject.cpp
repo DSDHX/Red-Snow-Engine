ed7c9e94eea72b687f3ad5758cc8c9a39a5d7643
DSDHX <56496118+DSDHX@users.noreply.github.com>
2025-11-08 02:48:16 -0800

    Weak 5

--- Framework/Graphics/Src/RenderObject.cpp
+++ Framework/Graphics/Src/RenderObject.cpp
@@ -20,13 +20,29 @@ void RenderGroup::Initialize(const std::filesystem::path& modelFilePath)
     const Model* model = ModelManager::Get()->GetModel(modelId);
     ASSERT(model != nullptr, "RenderGroup: model %s did not load", modelFilePath.u8string().c_str());
 
+    auto TryLoadTexture = [](const auto& textureName)-> TextureId
+    {
+        if (textureName.empty())
+        {
+            return 0;
+        }
+        
+        return TextureManager::Get()->LoadTexture(textureName, false);
+    };
+
     for (const Model::MeshData& meshData : model->meshData)
     {
         RenderObject& renderObject = renderObjects.emplace_back();
         renderObject.meshBuffer.Initialize(meshData.mesh);
         if (meshData.materialIndex < model->materialData.size())
         {
-            
+            const Model::MaterialData& materialData = model->materialData[meshData.materialIndex];
+            renderObject.material = materialData.material;
+
+            renderObject.diffuseMapId = TryLoadTexture(materialData.diffuseMapName);
+            renderObject.specMapId = TryLoadTexture(materialData.specMapName);
+            renderObject.normalMapId = TryLoadTexture(materialData.normalMapName);
+            renderObject.bumpMapId = TryLoadTexture(materialData.bumpMapName);
         }
     }
 }
