aa9d1c3603a57e50b3af627992bd0ce35995ed8b
DSDHX <56496118+DSDHX@users.noreply.github.com>
2026-01-14 09:06:00 -0800

    Add animation system and HelloAnimation demo
    Introduced new animation system with Animation, AnimationBuilder, and Keyframe classes. Added HelloAnimation demo project with supporting source files and updated solution and project files to include new functionality.

---
+++ VGP334/14_HelloAnimation/GameState.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <RedSnowEngine/Inc/RedSnowEngine.h>
+
+class GameState : public RedSnowEngine::AppState
+{
+public:
+    void Initialize() override;
+    void Terminate() override;
+    void Update(float deltaTime) override;
+    void Render() override;
+    void DebugUI() override;
+
+private:
+    void UpdateCamera(float deltaTime);
+
+    RedSnowEngine::Graphics::Camera mCamera;
+    RedSnowEngine::Graphics::DirectionalLight mDirectionalLight;
+
+    RedSnowEngine::Graphics::RenderObject mRenderObject;
+    RedSnowEngine::Graphics::StandardEffecr mStandardEffect;
+
+    RedSnowEngine::Graphics::Animation mAnimation;
+    float mAnimationTime = 0.0f;
+};
